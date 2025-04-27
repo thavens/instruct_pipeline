5b6f75052fb90aa0babe8c2c8b33d6ced8edc71c
Michael Lavery <michael.lavery.2017@gmail.com>
2025-04-26 22:37:29 -0700

    add 'assistant\n' as additional tokens to be masked out of the loss

--- dev/qwen2.5_template.py
+++ dev/qwen2.5_template.py
@@ -21,10 +21,10 @@ TEMPLATE = """\
     {%- if (message.role == \"user\") or (message.role == \"system\" and not loop.first) %}
         {{- '<|im_start|>' + message.role + '\\n' + message.content + '<|im_end|>' + '\\n' }}
     {%- elif message.role == \"assistant\" %}
-        {{- '<|im_start|>' }}{% generation %}
-        {{- message.role }}
+        {{- '<|im_start|>' }}
+        {{- message.role + '\\n' }}{% generation %}
         {%- if message.content %}
-            {{- '\\n' + message.content }}
+            {{- message.content }}
         {%- endif %}
         {%- for tool_call in message.tool_calls %}
             {%- if tool_call.function is defined %}
@@ -55,18 +55,38 @@ TEMPLATE = """\
 """
 
 if __name__ == "__main__":
-    print(TEMPLATE)
     from transformers import AutoTokenizer
+
     tok = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
     tok2 = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
-    
+
     tok.chat_template = TEMPLATE
-    conv = [{'role': "user", "content": "can you use python to find 1+1?"}, {"role": "assistant", "content": "the answer is 2"}]
-    
-    output = tok.apply_chat_template(conv, add_generation_prompt=False, continue_final_message=False, return_dict=True, return_assistant_tokens_mask=True)
-    output2 = tok2.apply_chat_template(conv, add_generation_prompt=False, continue_final_message=False)
-    
+    conv = [
+        {"role": "user", "content": "can you use python to find 1+1?"},
+        {"role": "assistant", "content": "the answer is 2"},
+        {"role": "user", "content": "can you use python to find 1+1?"},
+        {"role": "assistant", "content": "the answer is 2"},
+    ]
+
+    output = tok.apply_chat_template(
+        conv,
+        add_generation_prompt=False,
+        continue_final_message=False,
+        return_dict=True,
+        return_assistant_tokens_mask=True,
+    )
+    output2 = tok2.apply_chat_template(
+        conv, add_generation_prompt=False, continue_final_message=False
+    )
+
     assert output["input_ids"] == output2
     print(output)
     print(tok.decode(output["input_ids"]))
-    
+    relevant_ids = [
+        input_id
+        for input_id, mask in zip(output["input_ids"], output["assistant_masks"])
+        if mask
+    ]
+
+    generation = tok.batch_decode([relevant_ids])
+    print(generation)
